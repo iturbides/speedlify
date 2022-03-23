b65f04e8a60a3a2419c33491b15caa133650f45f
Iturbides <iturbides@keemail.me>
2022-03-23 14:26:29 +0100

    Update and rename test-runners.js to artbarcelonamag.js

---
+++ _data/sites/artbarcelonamag.js
@@ -0,0 +1,10 @@
+module.exports = {
+	name: "ArtBarcelonaMAG", // optional, falls back to object key
+	description: "Marcos-Gonzalez Painter",
+	options: {
+		frequency: 60 * 23, // (in minutes), 23 hours
+	},
+	urls: [
+		"https://artbarcelonamag.com/"
+	]
+};
