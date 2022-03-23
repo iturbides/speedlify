e5091727ff901b145c040f437b7813270be00abc
Iturbides <iturbides@keemail.me>
2022-03-23 14:39:23 +0100

    Update and rename artbarcelonamag.js to iturbides.js

---
+++ _data/sites/iturbides.js
@@ -0,0 +1,12 @@
+module.exports = {
+	name: "Matias Iturbides", // optional, falls back to object key
+	description: "11ty sites by Iturbides",
+	options: {
+		frequency: 60 * 23, // (in minutes), 23 hours
+	},
+	urls: [
+		"https://pardeletras.com/",
+		"https://tottattoo.com/",
+		"https://artbarcelonamag.com/",
+	]
+};
