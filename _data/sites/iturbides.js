0f0e8279a1029674ebdbb05404e7881a7a989cde
Iturbides <iturbides@keemail.me>
2022-03-23 20:41:45 +0100

    Update iturbides.js

--- _data/sites/iturbides.js
+++ _data/sites/iturbides.js
@@ -1,8 +1,16 @@
 module.exports = {
 	name: "Matias Iturbides", // optional, falls back to object key
 	description: "11ty sites by Iturbides",
+	// skip if localhost
+	// skip if this is a new fork of the speedlify (not Zach’s)
+	skip: !process.env.CONTEXT || process.env.SITE_NAME !== "speedlify",
 	options: {
-		frequency: 60 * 23, // (in minutes), 23 hours
+		frequency: 60 * 23, // 24 hours
+		// Use "run" if the sites don’t share assets on the same origin
+		//           and we can reset chrome with each run instead of
+		//           each site in every run (it’s faster)
+		// Use "site" if sites are all on the same origin and share assets.
+		freshChrome: "run"
 	},
 	urls: [
 		"https://pardeletras.com/",
