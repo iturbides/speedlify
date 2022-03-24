9262e7f6501572c5204fe8ac3449976e956dfbc8
Iturbides <iturbides@keemail.me>
2022-03-24 19:00:33 +0100

    Update pardeletras.js

--- _data/sites/pardeletras.js
+++ _data/sites/pardeletras.js
@@ -1,6 +1,6 @@
 module.exports = {
 	name: "Matias Iturbides", // optional, falls back to object key
-	description: "11ty sites by Iturbides",
+	description: "Pardeletras",
 	// skip if localhost
 	// skip if this is a new fork of the speedlify (not Zach’s)
 	skip: !process.env.CONTEXT || process.env.SITE_NAME !== "speedlify",
